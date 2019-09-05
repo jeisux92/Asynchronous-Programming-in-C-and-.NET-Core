293511607388123cc0c6aadc5fc586baeaf13353
jeisux92 <jei.sum41@gmail.com>
2019-09-04 22:13:23 -0500

    Introducing to Asynchronous Programming

---
+++ AsyncCoin/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+namespace AsyncCoin
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            AsyncCoinManager coinManager = new AsyncCoinManager();
+            await coinManager.AcquireAsyncCoin();
+            Console.ReadLine();
+        }
+    }
+}
